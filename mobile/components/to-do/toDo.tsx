7375c6f350d533b7091d8507e4b9729e60abe9cc
Jorge Perdomo <jorge.perdomo@vts.com>
2024-12-04 15:19:32 -0800

    feat: add to-do item to list

--- mobile/components/to-do/toDo.tsx
+++ mobile/components/to-do/toDo.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Alert } from "react-native";
 import { ToDoInput } from "@/components/to-do/ToDoInput";
 import { ToDoList } from "@/components/to-do/ToDoList";
 
@@ -8,12 +10,28 @@ type ToDo = {
 };
 
 export default function ToDo() {
-  const toDoListTest: ToDo[] = [{ id: 1, text: "testing", completed: false }];
+  const [toDos, setToDos] = useState<ToDo[]>([]);
+
+  const handleOnAddToDo = (toDoInput: string) => {
+    const duplicateToDo = toDos.some(
+      (value) => toDoInput.trim() === value.text
+    );
+
+    if (duplicateToDo) {
+      Alert.alert("This To-Do item already exists.");
+      return;
+    }
+
+    setToDos([
+      { id: Date.now(), text: toDoInput.trim(), completed: false },
+      ...toDos,
+    ]);
+  };
 
   return (
     <>
-      <ToDoInput onAddToDo={() => {}} />
-      <ToDoList toDos={toDoListTest} />
+      <ToDoInput onAddToDo={handleOnAddToDo} />
+      <ToDoList toDos={toDos} />
     </>
   );
 }
