66064132fcda6d3a15727098af1ea27a1b67e693
Jorge Perdomo <jorge.perdomo@vts.com>
2024-12-05 12:29:08 -0800

    feat: add description and timestamps to to-do item

--- backend/src/models/ToDo.ts
+++ backend/src/models/ToDo.ts
@@ -3,14 +3,25 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface IToDo extends Document {
   userId: mongoose.Types.ObjectId;
   title: string;
+  description: string;
   completed: boolean;
 }
 
-const toDoSchema = new Schema<IToDo>({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  title: { type: String, required: true },
-  completed: { type: Boolean, default: false },
-});
+const toDoSchema = new Schema<IToDo>(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: { type: String, required: true },
+    description: { type: String, required: false },
+    completed: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const ToDo = mongoose.model<IToDo>("ToDo", toDoSchema);
 
