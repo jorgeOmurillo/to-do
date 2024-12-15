d29033bccc1315ea0a567672f67cde34b220ca1b
Jorge Perdomo <jorge.perdomo@vts.com>
2024-12-14 16:32:49 -0800

    feat: update register page style

--- mobile/app/register.tsx
+++ mobile/app/register.tsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
-import { router } from "expo-router";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { router, Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 import { useSession } from "@/context";
 
 export default function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isPasswordSecure, setIsPasswordSecure] = useState(true);
   const [error, setError] = useState("");
   const { register } = useSession();
 
-  const handleRegister = async () => {
+  const handleOnRegisterPress = async () => {
     try {
       await register({ username, password });
       router.replace("/(app)/(tabs)");
@@ -23,24 +31,57 @@ export default function Register() {
     }
   };
 
+  const handleOnLoginPress = () => {
+    router.navigate("/login");
+  };
+
   return (
     <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          headerShown: true,
+          headerTitleAlign: "left",
+          headerBackVisible: true,
+          headerBackButtonMenuEnabled: false,
+          headerTintColor: "#7C4DFF",
+          headerTitle() {
+            return <></>;
+          },
+        }}
+      />
+
       <Text style={styles.title}>Register</Text>
       <TextInput
         style={styles.input}
-        autoCapitalize="none"
         placeholder="Username"
         value={username}
         onChangeText={setUsername}
+        autoCapitalize="none"
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
-      <Button title="Register" onPress={handleRegister} />
+      <View style={styles.passwordInput}>
+        <TextInput
+          style={styles.passwordTextInput}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={isPasswordSecure}
+        />
+        <TouchableOpacity
+          onPress={() => setIsPasswordSecure(!isPasswordSecure)}
+        >
+          <Ionicons
+            name={isPasswordSecure ? "eye" : "eye-off"}
+            size={20}
+            color="#9286F3"
+          />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={handleOnRegisterPress}>
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleOnLoginPress}>
+        <Text style={styles.linkText}>Already have an account? Login</Text>
+      </TouchableOpacity>
       {error.length > 0 && <Text style={styles.errorText}>{error}</Text>}
     </View>
   );
@@ -49,23 +90,54 @@ export default function Register() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     padding: 20,
+    justifyContent: "center",
+    backgroundColor: "#f9f9f9",
   },
   title: {
     fontSize: 24,
+    fontWeight: "bold",
     marginBottom: 20,
+    textAlign: "center",
   },
   input: {
-    width: "100%",
-    padding: 10,
-    marginVertical: 10,
     borderWidth: 1,
     borderColor: "#ccc",
-    borderRadius: 5,
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 15,
+    backgroundColor: "#fff",
+  },
+  passwordInput: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    marginBottom: 15,
+    backgroundColor: "#fff",
+  },
+  passwordTextInput: {
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "#7C4DFF",
+    borderRadius: 8,
+    padding: 12,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  linkText: {
+    color: "#7C4DFF",
+    marginTop: 15,
+    textAlign: "center",
   },
   errorText: {
+    textAlign: "center",
     fontSize: 16,
     marginTop: 20,
     color: "red",
