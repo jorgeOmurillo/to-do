af2fee6fffa9214b4cd8398c2e256f29aa00e4ae
Jorge Perdomo <jorge.perdomo@vts.com>
2024-12-14 16:11:12 -0800

    feat: update login page styles

--- mobile/app/login.tsx
+++ mobile/app/login.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
-import { Link, router } from "expo-router";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { router } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
 import { useSession } from "@/context";
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isPasswordSecure, setIsPasswordSecure] = useState(true);
   const [error, setError] = useState("");
   const { logIn } = useSession();
 
-  const handleLogin = async () => {
+  const handleOnLoginPress = async () => {
     try {
       await logIn({ username, password });
       router.replace("/(app)/(tabs)");
@@ -22,25 +31,45 @@ export default function Login() {
     }
   };
 
+  const handleOnRegisterPress = async () => {
+    router.navigate("/register");
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
       <TextInput
         style={styles.input}
         placeholder="Username"
-        autoCapitalize="none"
         value={username}
         onChangeText={setUsername}
+        autoCapitalize="none"
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
-      <Button title="Login" onPress={handleLogin} />
-      <Link href="/register">Register</Link>
+      <View style={styles.passwordInput}>
+        <TextInput
+          style={styles.passwordTextInput}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={isPasswordSecure}
+        />
+        <TouchableOpacity
+          onPress={() => setIsPasswordSecure(!isPasswordSecure)}
+        >
+          <Ionicons
+            name={isPasswordSecure ? "eye" : "eye-off"}
+            size={20}
+            color="#9286F3"
+          />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={handleOnLoginPress}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleOnRegisterPress}>
+        <Text style={styles.linkText}>Don't have an account? Register</Text>
+      </TouchableOpacity>
       {error.length > 0 && <Text style={styles.errorText}>{error}</Text>}
     </View>
   );
@@ -49,23 +78,54 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     padding: 20,
+    justifyContent: "center",
+    backgroundColor: "#f9f9f9",
   },
   title: {
     fontSize: 24,
+    fontWeight: "bold",
     marginBottom: 20,
+    textAlign: "center",
   },
   input: {
-    width: "100%",
-    padding: 10,
-    marginVertical: 10,
     borderWidth: 1,
     borderColor: "#ccc",
-    borderRadius: 5,
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 15,
+    backgroundColor: "#fff",
+  },
+  passwordInput: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    marginBottom: 15,
+    backgroundColor: "#fff",
+  },
+  passwordTextInput: {
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "#7C4DFF",
+    borderRadius: 8,
+    padding: 12,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  linkText: {
+    color: "#7C4DFF",
+    marginTop: 15,
+    textAlign: "center",
   },
   errorText: {
+    textAlign: "center",
     fontSize: 16,
     marginTop: 20,
     color: "red",
